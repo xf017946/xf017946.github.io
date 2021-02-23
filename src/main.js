4bb4df66539dcb3ff54573ef300ba6479c900d4f
fei4.chen <fei4.chen@zhangmeng.com>
2021-02-24 01:06:14 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import { Swipe, SwipeItem } from 'vant'
+createApp(App).use(store).use(router).use(Swipe).use(SwipeItem).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,123 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-swipe ref="swiper" :show-indicators="false" @change="onChange">
+      <van-swipe-item v-for="item of goodsData" :key="item.imgUrl">
+        <div class="wrap-swiper-item">
+          <img class="goods" :src="item.imgUrl" alt="">
+          <p class="desc">{{ item.desc}}</p>
+          <p class="price">{{ item.price ? `￥${item.price}`: '' }}</p>
+        </div>
+      </van-swipe-item>
+    </van-swipe>
+    <div>
+      <p class="label">款式</p>
+      <div class="wrap-labels">
+        <span 
+          v-for="(item, index) of goodsData"
+          :key="item.name" :class="[{active: index === current}, 'labels']"
+          @click="change(index)"
+        >{{ item.name }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import { ref } from 'vue'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    const current = ref(0)
+    const onChange = (index) => {
+      current.value = index;
+    }
+    return {
+      goodsData: [
+        {
+          imgUrl: require('../assets/1.jpeg'),
+          name: '汽车名称1xx',
+          price: '10',
+          desc: '这是汽车名称1xx'
+        },
+        {
+          imgUrl: require('../assets/2.jpeg'),
+          name: '汽车名称2xx',
+          price: '20元',
+          desc: '这是汽车名称2xx'
+        }
+      ],
+      current,
+      onChange
+    };
+  },
+  methods: {
+    change(index){
+      if (index !== this.current) {
+        this.$refs.swiper.swipeTo(index);
+      }
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  .label {
+    margin-left: 10px;
+    text-align: left;
+  }
+  .wrap-labels {
+    display: flex;
+    justify-content: flex-start;
+    margin: 10px 10px;
+  }
+  .labels {
+    display: inline-block;
+    padding: 2px 4px;
+    margin-right: 8px;
+    background: #efefef;
+    border-radius: 4px;
+    color: #000;
+    &.active {
+      color: #fff;
+      background: #e80a0a;
+    }
+  }
+  .goods {
+    width: 100%;
+    height: 280px;
+    object-fit: cover;
+  }
+  .wrap-swiper-item {
+    font-size: 0;
+    position: relative;
+    .desc {
+      font-size: 14px;
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 20px;
+      color: #fff;
+      text-align: left;
+      padding-left: 10px;
+      background: rgba(0,0,0,0.3);
+      line-height: 20px;
+    }
+    .price {
+      font-size: 14px;
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      line-height: 20px;
+      color: #ff7676;
+      text-align: left;
+      padding-left: 10px;
+      background: rgba(0,0,0,0.3);
+    }
+  }
+</style>
